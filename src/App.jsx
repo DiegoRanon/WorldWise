1e57c6db598f022b6ca4ce2ffee343bf16ba6875
Diego Ranon <Mastercelis8891@gmail.com>
2025-05-09 15:06:46 -0400

    Linking routes with NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Product from './pages/Product'
-import Pricing from './pages/Pricing'
-import Homepage from './pages/Homepage'
-import PageNotFound from './pages/PageNotFound'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
-    <BrowserRouter> 
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
-        <Route path='pricing' element={<Pricing />} />
-        <Route path='*' element={<PageNotFound />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function PageNotFound() {
-  return (
-    <div>
-      Page not found
-    </div>
-  )
+  return <div>Page not found</div>;
 }
