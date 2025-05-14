9ec24d26712adc98568767d0a824a2d2bbc469ee
Diego Ranon <Mastercelis8891@gmail.com>
2025-05-14 10:10:44 -0400

    Finishing the City View

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import styles from './CityItem.module.css'
-import { Link, Links } from 'react-router-dom';
+import React from "react";
+import styles from "./CityItem.module.css";
+import { Link, Links } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -10,17 +11,21 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 export default function CityItem({ city }) {
-    const {cityName, emoji, date, id, position } = city;
-    console.log(position);
+  const { currentCity } = useCities();
+  const { cityName, emoji, date, id, position } = city;
+  console.log(position);
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+      <Link
+        className={`${styles.cityItem} ${id === currentCity.id ? styles["cityItem--active"] : ""}`}
+        to={`/app/cities/${id}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
         <button className={styles.deleteBtn}>&times;</button>
       </Link>
     </li>
-  )
+  );
 }
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,6 +6,7 @@ const BASE_URL = "http://localhost:8000";
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState();
 
   useEffect(function () {
     async function fetchCities() {
@@ -23,11 +24,26 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        currentCity,
+        getCity
       }}
     >
       {children}
@@ -36,9 +52,10 @@ function CitiesProvider({ children }) {
 }
 
 function useCities() {
-    const context = useContext(CitiesContext);
-    if(context === undefined) throw new Error('CitiesContext was used outside the CitiesProvider')
-    return context;
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
 }
 
-export { CitiesProvider, useCities};
+export { CitiesProvider, useCities };
